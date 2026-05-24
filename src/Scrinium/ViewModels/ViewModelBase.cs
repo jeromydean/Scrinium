b03a522883c68b3b628e62e9102c881e94ef2e03
jeromydean <jeromydean@gmail.com>
2026-05-24 10:51:13 -0400

    Add default Avalonia MVVM template
    Updated nuget references

---
+++ src/Scrinium/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Scrinium.ViewModels
+{
+  public abstract class ViewModelBase : ObservableObject
+  {
+  }
+}
---
+++ src/Scrinium/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Scrinium.Views
+{
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+  }
+}
